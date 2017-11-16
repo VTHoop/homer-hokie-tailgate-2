c8271ab7278a801f0fc10bab1422fe95959f6e25
Patrick Hooper <patrick.hooper.m@outlook.com>
2017-11-16 14:39:18 -0500

    adding back yes_attendance

--- src/models/games/views.py
+++ src/models/games/views.py
@@ -13,7 +13,7 @@ games_blueprint = Blueprint('games', __name__)
 @games_blueprint.route('/detail/<int:game_num>')
 def detail(game_num):
     this_game = Game.get_game_by_num(game_num)
-    # yes_attendance = UserGame.get_attendance_by_game_and_status(game_num, 'Yes') 
+    yes_attendance = UserGame.get_attendance_by_game_and_status(game_num, 'Yes') 
     this_game.date = datetime.strftime(datetime.strptime(this_game.date, "%m/%d/%Y"), "%B %d")
-    return render_template("games/game_detail.jinja2", game=this_game)
+    return render_template("games/game_detail.jinja2", game=this_game, yes_attendance=yes_attendance)
 
