2bacaf791eb267baa4a5f002063945c1fd214a62
Patrick Hooper <patrick.hooper.m@outlook.com>
2017-11-16 13:56:15 -0500

    removing game and status func

--- src/models/user_games/user_game.py
+++ src/models/user_games/user_game.py
@@ -25,9 +25,9 @@ class UserGame(object):
     def get_attendance_by_user(cls, user):
         return [cls(**elem) for elem in Database.find_and_sort(UserGameConstants.COLLECTION,  {"user": user }, "game", 1)]
     
-    @classmethod
-    def get_attendance_by_game_and_status(cls, game, attendance):
-        return [cls(**elem) for elem in Database.find(UserGameConstants.COLLECTION,  {"game": game, "attendance": attendance}]
+#    @classmethod
+#    def get_attendance_by_game_and_status(cls, game, attendance):
+#       return [cls(**elem) for elem in Database.find(UserGameConstants.COLLECTION,  {"game": game, "attendance": attendance}]
 
     def save_to_mongo(self):
         Database.update(UserGameConstants.COLLECTION, {"_id": self._id}, self.json())
