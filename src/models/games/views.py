7443d0cef50ca03ac945c1c296f639bb686371d4
Patrick Hooper <patrick.hooper.m@outlook.com>
2017-11-16 14:56:19 -0500

    adding maybe and no attendance

--- src/models/games/views.py
+++ src/models/games/views.py
@@ -13,7 +13,9 @@ games_blueprint = Blueprint('games', __name__)
 @games_blueprint.route('/detail/<int:game_num>')
 def detail(game_num):
     this_game = Game.get_game_by_num(game_num)
-    yes_attendance = UserGame.get_attendance_by_game_and_status(game_num, 'Yes') 
+    yes_attendance = UserGame.get_attendance_by_game_and_status(game_num, 'Yes')
+    maybe_attendance = UserGame.get_attendance_by_game_and_status(game_num, 'Maybe') 
+    no_attendance = UserGame.get_attendance_by_game_and_status(game_num, 'No') 
     this_game.date = datetime.strftime(datetime.strptime(this_game.date, "%m/%d/%Y"), "%B %d")
-    return render_template("games/game_detail.jinja2", game=this_game, yes_attendance=yes_attendance)
+    return render_template("games/game_detail.jinja2", game=this_game, yes_attendance=yes_attendance, maybe_attendance=maybe_attendance, no_attendance=no_attendance)
 
