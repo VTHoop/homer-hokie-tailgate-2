65d663a5aef8e2e126dd8bce4b6e99871ad9ce65
Patrick Hooper <patrick.hooper.m@outlook.com>
2018-03-12 21:16:49 -0400

    updated interface for game detail

---
+++ src/static/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $('.js--game-details').waypoint(function(direction) {
+       if (direction == 'down') {
+           $('nav').addClass('sticky');
+       } else {
+           $('nav').removeClass('sticky');
+       }
+    },{
+        offset: '21%;'
+    });
+
+    
+    /*scroll on button */
+    $('.js--scroll-to-menu').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-menu').offset().top -100}, 1000); 
+    });
+    
+    $('.js--scroll-to-attendance').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-attendance').offset().top -100}, 1000); 
+    });
+    
+    $('.js--scroll-to-preview').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-preview').offset().top -100}, 1000); 
+    });
+    
+    $('.js--scroll-to-tickets').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-tickets').offset().top -100}, 1000); 
+    });
+    
+});
