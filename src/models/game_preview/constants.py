5d70c37a18a7fb8a21781ab13624e4f0daa75d86
Patrick Hooper <patrick.hooper.m@outlook.com>
2018-08-12 07:51:29 -0400

    hide Mailgun details

--- src/models/game_preview/constants.py
+++ src/models/game_preview/constants.py
@@ -4,7 +4,7 @@ __author__ = 'hooper-p'
 
 COLLECTION = 'game_preview'
 
-URL = 'https://api.mailgun.net/v3/sandboxc67849019a20419c88582d1ba37e8c4a.mailgun.org/messages'
+URL = os.environ.get("MAILGUN_URL")
 API_KEY = os.environ.get("MAILGUN_API")
 
 FROM = 'Mailgun Sandbox <postmaster@sandbox67849019a20419c88582d1ba37e8c4a.mailgun.org>'
