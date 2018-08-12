0cedffafcc31b31c306cb106c05ab5d6dbd8a220
Patrick Hooper <patrick.hooper.m@outlook.com>
2018-08-12 07:49:05 -0400

    allow offline user creation; stop admin created users from logging in

--- src/models/game_preview/constants.py
+++ src/models/game_preview/constants.py
@@ -1,8 +1,11 @@
+import os
+
 __author__ = 'hooper-p'
 
 COLLECTION = 'game_preview'
 
 URL = 'https://api.mailgun.net/v3/sandboxc67849019a20419c88582d1ba37e8c4a.mailgun.org/messages'
-API_KEY = 'd6f076e5ee4e15adede11c12c2bac9b9-a5d1a068-fcc4e114'
+API_KEY = os.environ.get("MAILGUN_API")
+
 FROM = 'Mailgun Sandbox <postmaster@sandbox67849019a20419c88582d1ba37e8c4a.mailgun.org>'
 
