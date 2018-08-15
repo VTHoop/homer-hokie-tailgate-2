3fd89e85022ed498e2445f5a453a9bd672bb1018
Patrick Hooper <patrick.hooper.m@outlook.com>
2018-08-15 05:53:00 -0400

    cleanup initialization

--- src/common/database.py
+++ src/common/database.py
@@ -6,14 +6,13 @@ import pymongo
 
 
 class Database(object):
-    # URI = "mongodb://127.0.0.1:27017"
     URI = os.environ.get("MONGODB_URI")
     DATABASE = None
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client['homerhokie']
 
     @staticmethod
     def insert(collection, data):
