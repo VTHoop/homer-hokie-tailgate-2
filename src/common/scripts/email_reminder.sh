450f64a10e95f0baee54df388fe4d77b4d9a1a90
Patrick Hooper <patrick.hooper.m@outlook.com>
2018-08-30 11:34:43 -0400

    get correct service

--- src/common/scripts/email_reminder.sh
+++ src/common/scripts/email_reminder.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=/var/www/html/homer-hokie
-/var/www/html/homer-hokie/venv/bin/python /var/www/html/pricing-service/src/common/services/email_reminder.py
+/var/www/html/homer-hokie/venv/bin/python /var/www/html/homer-hokie/src/common/services/email_reminder.py
